94181b938fc1ef8984199c943b6fb9c9760c5dfe
Shell Raiser <ksshailesh05@gmail.com>
2022-07-10 18:48:31 +0530

    test run for sanscript

---
+++ sanscript test.js
@@ -0,0 +1,6 @@
+const Sanscript = require("@sanskrit-coders/sanscript")
+
+console.log(Sanscript.t('ಹೇಲ್ಲೋ', 'kannada', 'hk'));
+console.log(Sanscript.t('హేల్లో', 'telugu', 'hk'));
+console.log(Sanscript.t('ஹேல்லோ', 'tamil', 'hk'));
+
