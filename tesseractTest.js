668f5d43fbccdf69ad7ec77496384a39d9afe34f
shell-raiser <ksshailesh05@gmail.com>
2022-07-12 16:38:14 +0000

    tesseractjsDemo

---
+++ tesseractTest.js
@@ -0,0 +1,10 @@
+// import Tesseract from 'tesseract.js';
+// import Tesseract from 'tesseract.js';
+const Tesseract = require('tesseract.js)
+Tesseract.recognize(
+    'https://tesseract.projectnaptha.com/img/eng_bw.png',
+    'eng',
+    { logger: m => console.log(m) }
+).then(({ data: { text } }) => {
+    console.log(text);
+})
