e34f357e4baa852e733708031778245e960dce53
shell-raiser <ksshailesh05@gmail.com>
2022-07-14 01:24:58 +0000

    remove temp file

--- tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-ar outputImages1 = pdf2img.convert( theFile
-        //     , {
-        //         width: 2480, //Number in px
-        //         height: 3508, // Number in px
-        //     }
-        // );
